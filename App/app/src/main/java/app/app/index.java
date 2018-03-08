d22faf81d4164a78009e62977d8fee3b2cdfd60e
Luis Fernando Vásquez <luisf96v@gmail.com>
2018-03-08 12:27:18 -0600

    Navigation Drawer Added

--- App/app/src/main/java/app/app/index.java
+++ App/app/src/main/java/app/app/index.java
@@ -1,7 +1,10 @@
 package app.app;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class index extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class index extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_index);
+        drawer = (DrawerLayout) findViewById(R.id.act_index);
+        toggle = new ActionBarDrawerToggle(this, drawer, R.string.open,  R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
 }
